2949c26093ee3155ab85b197281a936ffe0a7159
Simoncar <simoncar@gmail.com>
2020-04-30 15:29:20 +0800

    Updates

---
+++ precache-manifest.df82225441250f9211c8b7970da26e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6af8d2a9da0231d137b1",
+    "url": "/jolimont/static/js/runtime~main.6af8d2a9.js"
+  },
+  {
+    "revision": "31b34353dae4aa951742",
+    "url": "/jolimont/static/js/main.31b34353.chunk.js"
+  },
+  {
+    "revision": "826777a9fa731c1a219a",
+    "url": "/jolimont/static/js/2.826777a9.chunk.js"
+  },
+  {
+    "revision": "31b34353dae4aa951742",
+    "url": "/jolimont/static/css/main.32994709.chunk.css"
+  },
+  {
+    "revision": "826777a9fa731c1a219a",
+    "url": "/jolimont/static/css/2.ccedd8d1.chunk.css"
+  },
+  {
+    "revision": "519e0f83d01acf569312c8d23f511578",
+    "url": "/jolimont/index.html"
+  }
+];
