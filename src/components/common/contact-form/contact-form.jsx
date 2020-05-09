e3b58d20abe37f69d67b8b95392a1f8835095dd7
Simoncar <simoncar@gmail.com>
2020-05-09 10:39:34 +0800

    footer text

--- src/components/common/contact-form/contact-form.jsx
+++ src/components/common/contact-form/contact-form.jsx
@@ -1,81 +1,152 @@
-import React, {Component} from 'react';
-import Form from 'react-validation/build/form';
-import Input from 'react-validation/build/input';
-import CheckButton from 'react-validation/build/button';
-import Textarea from 'react-validation/build/textarea'
-import {isEmail, isEmpty, isMobilePhone} from 'validator';
-import './contact-form.scss';
+import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import Textarea from "react-validation/build/textarea";
+import { isEmail, isEmpty, isMobilePhone } from "validator";
+import "./contact-form.scss";
 
 class ContactForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            phone: '',
-            email: '',
-            content: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      phone: "",
+      email: "",
+      content: "",
+    };
+  }
+
+  required = (value) => {
+    if (isEmpty(value)) {
+      return <small className="form-text text-danger">This field is required</small>;
     }
+  };
 
-    required = (value) => {
-        if (isEmpty(value)) {
-            return <small className="form-text text-danger">This field is required</small>;
-        }
+  email = (value) => {
+    if (!isEmail(value)) {
+      return <small className="form-text text-danger">Invalid email format</small>;
     }
+  };
 
-    email = (value) => {
-        if (!isEmail(value)) {
-            return <small className="form-text text-danger">Invalid email format</small>;
-        }
+  phone = (value) => {
+    if (!isMobilePhone(value)) {
+      return <small className="form-text text-danger">Invalid Phone number format</small>;
     }
+  };
 
-    phone = (value) => {
-        if (!isMobilePhone(value)) {
-            return <small className="form-text text-danger">Invalid Phone number format</small>;
-        }
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.form.validateAll();
+
+    if (this.checkBtn.context._errors.length === 0) {
+      alert(
+        " success!" +
+          "\n name: " +
+          this.state.name +
+          "\n email: " +
+          this.state.email +
+          "\n phone:" +
+          this.state.phone +
+          "\n content:" +
+          this.state.content +
+          ""
+      );
     }
+  };
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        this.form.validateAll();
+  render_backup() {
+    return (
+      <Form
+        className="form-group-v1"
+        onSubmit={(e) => this.onSubmit(e)}
+        ref={(c) => {
+          this.form = c;
+        }}>
+        <div className="row">
+          <div className="col-lg-6 col-md-12">
+            <div className="form-group">
+              <Input
+                name="name"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                value={this.state.name}
+                type="text"
+                placeholder="Your name *"
+                className="form-control"
+                validations={[this.required]}
+              />
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-12">
+            <div className="form-group">
+              <Input
+                name="phone"
+                onChange={(e) => {
+                  this.setState({ phone: e.target.value });
+                }}
+                value={this.state.phone}
+                type="text"
+                placeholder="Phone number *"
+                className="form-control"
+                validations={[this.required, this.phone]}
+              />
+            </div>
+          </div>
+        </div>
 
-        if (this.checkBtn.context._errors.length === 0) {
-            alert(' success!' + '\n name: ' + this.state.name + '\n email: ' + this.state.email + '\n phone:' + this.state.phone + '\n content:' + this.state.content + '');
-        }
-    }
+        <div className="form-group">
+          <Input
+            name="email"
+            onChange={(e) => {
+              this.setState({ email: e.target.value });
+            }}
+            value={this.state.email}
+            type="email"
+            placeholder="Your e-mail *"
+            className="form-control"
+            validations={[this.required, this.email]}
+          />
+        </div>
 
-    render() {
-        return (
-            <Form className="form-group-v1" onSubmit={e => this.onSubmit(e)} ref={c => {
-                this.form = c
-            }}>
-                <div className="row">
-                    <div className="col-lg-6 col-md-12">
-                        <div className="form-group">
-                            <Input name="name" onChange={(e) => {this.setState({name: e.target.value})}} value={this.state.name} type="text" placeholder="Your name *" className="form-control" validations={[this.required]}/>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-md-12">
-                        <div className="form-group">
-                            <Input name="phone" onChange={(e) => {this.setState({phone: e.target.value})}}  value={this.state.phone} type="text" placeholder="Phone number *" className="form-control" validations={[this.required, this.phone]}/>
-                        </div>
-                    </div>
-                </div>
+        <div className="form-group">
+          <Textarea
+            onChange={(e) => {
+              this.setState({ content: e.target.value });
+            }}
+            value={this.state.content}
+            className="form-control"
+            placeholder="How can we help you ?"
+            rows="3"
+            validations={[this.required]}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-4 btn-block">
+          Submit
+        </button>
+        <CheckButton
+          style={{ display: "none" }}
+          ref={(c) => {
+            this.checkBtn = c;
+          }}
+        />
+      </Form>
+    );
+  }
 
-                <div className="form-group">
-                    <Input name="email" onChange={(e) => {this.setState({email: e.target.value})}} value={this.state.email} type="email" placeholder="Your e-mail *" className="form-control" validations={[this.required, this.email]}/>
-                </div>
+  render() {
+    return (
+      <div className="section-1 text-center">
+        <h6 className="text-uppercase">BOOKINGS</h6>
+        <p>Only a select few bookings are taken each year.</p>
 
-                <div className="form-group">
-                    <Textarea onChange={(e) => {this.setState({content: e.target.value})}}  value={this.state.content} className="form-control" placeholder="How can we help you ?" rows="3" validations={[this.required]}/>
-                </div>
-                <button type="submit" className="btn btn-primary mt-4 btn-block">Submit</button>
-                <CheckButton style={{display: 'none'}} ref={c => {
-                    this.checkBtn = c
-                }}/>
-            </Form>
-        )
-    }
+        <h6 className="text-uppercase">E-MAIL:</h6>
+        <a href={"mailto:simoncar+jolimont@gmail.com?subject=106 Jolimont Road"}>simoncar@gmail.com</a>
+        <p></p>
+      </div>
+    );
+  }
 }
 
-export default ContactForm
+export default ContactForm;
--- src/components/common/footers/footer-one.jsx
+++ src/components/common/footers/footer-one.jsx
@@ -19,45 +19,7 @@ class FooterOne extends Component {
             <h3 className="text-uppercase">ARE YOU INTERESTED</h3>
             <h2 className="text-uppercase">IT'S TIME TO DISCOVER</h2>
             <h3 className="text-uppercase">THE BUILDING</h3>
-          </div>
-        </div>
-
-        {/*Section 2*/}
-        <div className="section-2">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-4 col-md-12">
-                <h6 className="heading-title">BOOKINGS</h6>
-                <p>Only a select few bookings are taken each year.</p>
-                <div className="icon-list footer-address">
-                  <ul>
-                    <li>
-                      <span className="icon-list-icon">PHONE:</span>
-                      <span className="icon-list-text">123--1234-1234</span>
-                    </li>
-                    <li>
-                      <span className="icon-list-icon">E-MAIL:</span>
-                      <span className="icon-list-text">________@gmail.com</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="col-lg-1 col-md-12"></div>
-              <div className="col-lg-4 col-md-12">
-                <h6 className="heading-title">ENQUIRE</h6>
-                <ContactForm />
-              </div>
-            </div>
-          </div>
-        </div>
-        {/*Section #2*/}
-        {/*Section 3*/}
-        <div className="section-3">
-          <div className="container text-center">
-            <div className="row">
-              <div className="col-sm-12"></div>
-            </div>
+            <ContactForm />
           </div>
         </div>
       </footer>
